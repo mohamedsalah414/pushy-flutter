4ffc13a231937fa4e1738bf18307fb4b9c8979ad
Mohamed Salah <36240402+mohamedsalah414@users.noreply.github.com>
2025-02-17 12:21:40 +0200

    Update PushyPlugin.java

--- android/src/main/java/me/pushy/sdk/flutter/PushyPlugin.java
+++ android/src/main/java/me/pushy/sdk/flutter/PushyPlugin.java
@@ -30,7 +30,7 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+// import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import me.pushy.sdk.flutter.config.PushyChannels;
 import me.pushy.sdk.Pushy;
